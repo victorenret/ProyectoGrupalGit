e925de94609b3066131e65626d6c3f6cec3378a6
neigood <neifergonzalez@gmail.com>
2021-09-10 19:31:08 -0300

    version hora
    HORA

--- js/scrip.js
+++ js/scrip.js
@@ -30,3 +30,23 @@ window.addEventListener('scroll', function(){
     
 });
 
+window.onload = function() {
+    var fecha;
+    var hora;
+    
+    var d = new Date(),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+if (month.length < 2) 
+    month = '0' + month;
+if (day.length < 2) 
+    day = '0' + day;
+fechap=[day,month,year].join('-')
+
+
+
+    document.getElementById("ejecucion").innerHTML = fechap;
+
+};
