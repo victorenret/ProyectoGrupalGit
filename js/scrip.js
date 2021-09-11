d5389a979e7756fdf42d5e98af9ca55882c842de
Virgitm <90473087+Virgitm@users.noreply.github.com>
2021-09-11 12:06:43 -0300

    VersiónJS2
    Validación formulario por campos

--- js/scrip.js
+++ js/scrip.js
@@ -1,63 +1,111 @@
-let nav = document.getElementById('menSoc');
-
+let nav = document.getElementById("menSoc");
 
 function menus() {
+  let Desplazamiento_actual = window.pageYOffset;
 
-    let Desplazamiento_actual = window.pageYOffset;
-
-    if (Desplazamiento_actual >= 40) {
-        nav.classList.remove('bg-dark');
-        nav.className = ('bg-dark1');
-        nav.style.transition = '1s';
-
-    } else {
-        nav.classList.remove('bg-dark1');
-        nav.className = ('bg-dark');
-        nav.style.transition = '1s';
-    }
-
+  if (Desplazamiento_actual >= 40) {
+    nav.classList.remove("bg-dark");
+    nav.className = "bg-dark1";
+    nav.style.transition = "1s";
+  } else {
+    nav.classList.remove("bg-dark1");
+    nav.className = "bg-dark";
+    nav.style.transition = "1s";
+  }
 }
 
-window.addEventListener('load', function () {
-    menus();
-
+window.addEventListener("load", function () {
+  menus();
 });
 
-
-window.addEventListener('scroll', function () {
-    console.log(window.pageYOffset);
-    menus();
-
+window.addEventListener("scroll", function () {
+  console.log(window.pageYOffset);
+  menus();
 });
 
 function startfecha() {
-    var d = new Date(),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+  var d = new Date(),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-    if (month.length < 2)
-        month = '0' + month;
-    if (day.length < 2)
-        day = '0' + day;
-    fechap = [day, month, year].join('-')
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+  fechap = [day, month, year].join("-");
 
-    document.getElementById("fecha").innerHTML = fechap;
+  document.getElementById("fecha").innerHTML = fechap;
+}
+
+function startTime() {
+  today = new Date();
+  h = today.getHours();
+  m = today.getMinutes();
+  s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("reloj").innerHTML = h + ":" + m + ":" + s;
+  t = setTimeout("startTime()", 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+window.onload = function () {
+  startTime();
+  startfecha();
 };
 
-    function startTime() {
-        today = new Date();
-        h = today.getHours();
-        m = today.getMinutes();
-        s = today.getSeconds();
-        m = checkTime(m);
-        s = checkTime(s);
-        document.getElementById('reloj').innerHTML = h + ":" + m + ":" + s;
-        t = setTimeout('startTime()', 500);
+// document.addEventListener("DOMContentLoaded", function () {
+//   document
+//     .getElementById("formulario")
+//     .addEventListener("submit", validarFormulario);
+// });
+
+// function validarFormulario(evento) {
+//   evento.preventDefault();
+//   var usuario = document.getElementById("nombre").value;
+//   if (usuario.length == 0) {
+//     alert("Ingresar nombre");
+//     return;
+//   }
+//   var clave = document.getElementById("correo").value;
+//   if (clave.length == 0) {
+//     alert("Ingresar correo");
+//     return;
+//   }
+//   var clave = document.getElementById("mensaje").value;
+//   if (clave.length == 0) {
+//     alert("Ingresa tu mensaje");
+//     return;
+//   }
+//   this.submit();
+// }
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario);
+});
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    var usuario = document.getElementById('nombre').value;
+    if (usuario.length == 0) {
+        alert('Introducir texto en el campo Nombre');
+        return;
     }
 
-    function checkTime(i) { if (i < 10) { i = "0" + i; } return i; }
-    window.onload = function () { 
-        startTime(); 
-        startfecha(); 
+    var correo = document.getElementById('correo').value;
+    if (correo.length == 0) {
+        alert('Introducir texto en el campo Correo');
+        return;
     }
+
+    var mensaje = document.getElementById('mensaje').value;
+    if (mensaje.length == 0) {
+        alert('Introducir texto en el campo Mensaje');
+        return;
+    }
+    this.submit();
+}
